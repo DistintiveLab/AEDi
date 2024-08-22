10a78084274fa8430b50af661654b7eda25ce34a
Rodrigo Borges <rodrigo@borges.net.br>
2024-08-22 21:21:48 +0200

    adjust to create backend DB for project

--- R/run_app.R
+++ R/run_app.R
@@ -3,5 +3,8 @@
 #' @export
 #' @importFrom shiny shinyApp
 run_app <- function() {
+  if(!file.exists("dashboard_db.sqlite")) {
+    prepare_db()
+  }
   shiny::shinyApp(ui = app_ui(), server = app_server)
 }
