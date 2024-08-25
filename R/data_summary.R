4c2b0c0c291742f9e223ba6717597c213eab700b
Rodrigo Borges <rodrigo@borges.net.br>
2024-08-26 00:33:12 +0200

    adjust rbuildignore, add preliminary datasource type data, several minor adjustments to conform with build checks

--- R/data_summary.R
+++ R/data_summary.R
@@ -1,3 +1,7 @@
+utils::globalVariables(
+  c("fk", "geometry", "local_name", "n_pk", "name",
+    "nids", "ownpk", "refdate","size", "value"))
+
 quick_data_summary <- function(data) {
   print(dfSummary(data, graph.magnif = 0.8),
         method = 'render',
